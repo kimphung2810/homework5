36454cf7ec32c93e1f2cd6176d6148bb3aae2bcc
PHUNG TRAN <kimphung@csu.fullerton.edu>
2017-03-08 21:14:41 -0800

    commit

--- scripts/truck.js
+++ scripts/truck.js
@@ -14,6 +14,8 @@
         console.log('Delivering order for ' + customerId);
         this.db.remove(customerId);
     };
+    //add return Object.values(this.db.getAll()) so it will
+    //return value
     Truck.prototype.printOrders = function() {
         var customerIdArray = Object.keys(this.db.getAll());
         console.log('Truck #' + this.truckId + ' has pending orders:');
